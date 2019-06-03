81f084212599bf890d33df9f59cf31807b355265
Nina <nkonstan@gmail.com>
2019-06-03 22:09:26 +0100

    Add date picker to waiting list

--- waiting_list_project/waitinglist/forms.py
+++ waiting_list_project/waitinglist/forms.py
@@ -2,7 +2,14 @@ from django import forms
 from .models import Entry
 
 
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
+
 class EntryForm(forms.ModelForm):  # ModelForm: creates form automatically from model object
     class Meta:
         model = Entry
         exclude = ('created_at', 'updated_at',) # makes created and updated date and time uneditable
+        widgets = {
+            'date_of_birth_of_child': DateInput(),
+        }
